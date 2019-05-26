b43261ac32e2c87ae0f101439769ce7e52b39b25
csmith783 <31111470+csmith783@users.noreply.github.com>
2019-05-26 10:54:22 -0500

    Exercises 1 - 3
    Printing, Comments, numbers and math.

---
+++ lpthw_ex1.py
@@ -0,0 +1,10 @@
+# Exercise 1
+# A good first program
+
+print("Hello World!")
+print("Hello Again")
+print("I like typing like this.")
+print("This is fun.")
+print("Yay! Printing")
+print("I'd much rather you 'not'")
+print('I "Said" do not touch this.')
---
+++ lpthw_ex3.py
@@ -0,0 +1,54 @@
+# Exercise 3
+# Numbers and Math
+
+# + : plus, addition
+# - : minus, subtraction
+# / : slash, division
+# * : asterick, multiplication
+# % : percent, modulus
+# < : less than
+# > : greater than
+# <= : less than or equal to
+# >= : greater than or equal to 
+
+# Print out what we are going to be doing
+print("I will now count my chickens:")
+
+# Calculate the number of Hens
+print("Hens", 25 + 30 / 6)
+
+# Calculate the number of Roosters 
+print("Roosters", 100 - 25 * 3 % 4)
+
+# Print out what we will be doing
+print("Now I will count the eggs:")
+
+# Calculate the number of Eggs
+print(3 + 2 + 1 -5 + 4 % 2 - 1 /4 + 6)
+
+# check to see which is greater
+print("Is it tru that 3 + 2 < 5 - 7?")
+
+# Returns Boolean
+print(3 + 2 < 5 - 7)
+
+# Checks the values of 3 + 2
+print("What is 3 + 2?", 3 + 2)
+
+# Checks the value of 5 -7
+print("What is 5 - 7?", 5 - 7)
+
+# Realization
+print("Oh, that is why it is False.")
+
+# Print out
+print("How about some more?")
+
+# Is 5 greater than -2?
+print("Is it greater?", 5 > -2)
+
+# Is 5 greater than or equal to -2?
+print("Is it greater or equal to?", 5 >= -2)
+
+# is 5 less than or equal to -2?
+print("Is it less or equal?", 5 <= -2)
