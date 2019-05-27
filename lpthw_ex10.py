b5eb289fa50a65d2e76a240022e35f308a0d7fcb
csmith783 <corysmith783@gmail.com>
2019-05-27 11:46:45 -0500

    Testing Git integration with VSCode

--- lpthw_ex10.py
+++ lpthw_ex10.py
@@ -1,2 +1,28 @@
 # Exercise 10
 # What was that?
+
+# Common Escape Sequences
+# \\ backslash
+# \' single quote
+# \" double quote
+# \a ASCII bell 
+# \n line feed
+# \r carriage return
+# \t tab
+
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm a \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
